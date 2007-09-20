2910ed9a44b582ce273c0199d145951b3256bd40
bennygr <bennygr@9fabf1d2-8a3a-0410-866c-cb411183c2a1>
2007-09-20 08:30:16 +0000

    initial commit
    git-svn-id: http://svn.code.sf.net/p/hashlib2plus/code@2 9fabf1d2-8a3a-0410-866c-cb411183c2a1

---
+++ trunk/doc/examples/md5example.cpp
@@ -0,0 +1,21 @@
+#include <hashwrapper.h>
+#include <md5wrapper.h>
+#include <string>
+
+/*
+ * creating a wrapper object
+ */
+hashwrapper *myWrapper = new md5wrapper();
+
+/*
+ * create a hash from a string
+ */
+std::string hash1 = myWrapper->getHashFromString("Hello World");
+
+/*
+ * create a hash based on a file
+ */
+std::string hash2 = myWrapper->getHashFromFile("README.TXT");
+
+delete myWrapper;
+
