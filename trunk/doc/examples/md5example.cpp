12af9944e4d621e4777f93b0df7e8a1408cee0e1
bennygr <bennygr@9fabf1d2-8a3a-0410-866c-cb411183c2a1>
2008-01-09 13:06:18 +0000

    UPTODATE documentation for version 0.3
    git-svn-id: http://svn.code.sf.net/p/hashlib2plus/code@35 9fabf1d2-8a3a-0410-866c-cb411183c2a1

--- trunk/doc/examples/md5example.cpp
+++ trunk/doc/examples/md5example.cpp
@@ -1,21 +1,33 @@
 #include <hl_hashwrapper.h>
 #include <hl_md5wrapper.h>
 #include <string>
+#include <iostream>  //for "cerr"
 
 /*
  * creating a wrapper object
  */
 hashwrapper *myWrapper = new md5wrapper();
 
-/*
- * create a hash from a string
- */
-std::string hash1 = myWrapper->getHashFromString("Hello World");
+try
+{
+	/*
+	 * create a hash from a string
+	 */
+	std::string hash1 = myWrapper->getHashFromString("Hello World");
 
-/*
- * create a hash based on a file
- */
-std::string hash2 = myWrapper->getHashFromFile("README.TXT");
+	/*
+	 * create a hash based on a file
+	 */
+	std::string hash2 = myWrapper->getHashFromFile("README.TXT");
+}
+catch(hlException &e)
+{
+
+	std::cerr << "Error(" 
+		  << e.error_number()
+		  << "): "
+		  << e.erro_message()
+		  << std::endl;
+}
 
 delete myWrapper;
-
