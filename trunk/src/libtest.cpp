244ad95cf70d49c2d6419125d993e7fcca59a900
bennygr <bennygr@9fabf1d2-8a3a-0410-866c-cb411183c2a1>
2008-02-02 10:38:46 +0000

    g++ flags added. hashlibpp.h include-wrapper added.
    git-svn-id: http://svn.code.sf.net/p/hashlib2plus/code@46 9fabf1d2-8a3a-0410-866c-cb411183c2a1

--- trunk/src/libtest.cpp
+++ trunk/src/libtest.cpp
@@ -47,12 +47,7 @@
 
 //---------------------------------------------------------------------- 
 //hashlib++ includes
-#include <hl_hashwrapper.h>
-#include <hl_md5wrapper.h>
-#include <hl_sha1wrapper.h>
-#include <hl_sha256wrapper.h>
-#include <hl_sha384wrapper.h>
-#include <hl_sha512wrapper.h>
+#include <hashlibpp.h>
 
 //----------------------------------------------------------------------	
 //misc. includes
