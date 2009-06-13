6d5502d49420439a4d441caded9e39d41b9c98dc
bennygr <bennygr@9fabf1d2-8a3a-0410-866c-cb411183c2a1>
2009-06-13 09:40:06 +0000

    improved the example program
    git-svn-id: http://svn.code.sf.net/p/hashlib2plus/code@64 9fabf1d2-8a3a-0410-866c-cb411183c2a1

--- trunk/tools/cs.cpp
+++ trunk/tools/cs.cpp
@@ -29,27 +29,21 @@
 //---------------------------------------------------------------------- 
 
 /*
+ * hashlib++ example program.
  * This is a short example program which shows how to use hashlib++.
- * It produces hashes for a given string or file.
+ * It produces hash-values for a given string or file.
  *
- * THIS PROGRAMM IS JUST AN EXAMPLE HOW TO USE HASHLIB++ AND
- * NO FULL IMPLEMENTED SOFTWARE!
+ * usage: 
+ * [-t] [-s STRING | -f FILENAME] [-h HASHTYPE]
  *
- * usage:
+ *  -t       Tests the implementation on your system before.
+ *  -s       Creates a hash from a given string.
+ *  -f       Creates a hash from a given file.
+ *  -h       Hashfunction to use: md5,sha1,sha256,sha384,sha512.
  *
- * [-s string] [-f filename] [-h <hashfunction>]       
+ * hashlib++ - a simple hash library for C++
+ * Copyright (c) 2007,2008,2009 Benjamin Grüdelbach
  *
- * where <hashfunction> is one of the support hashfunctions:
- * md5
- * sha1
- * sha256
- * sha384
- * sha512
- *
- *
- * Benjamin Grüdelbach
- * 
- * Di 18 Sep 2007
  */
 
 
@@ -65,6 +59,7 @@
 //----------------------------------------------------------------------	
 //prototypes
 void printUsage (void);
+hashwrapper* factoryMethod(std::string htype);
 
 //----------------------------------------------------------------------	
 
@@ -72,11 +67,11 @@ int main ( int argc, char **argv)
 {
 	bool bCreateFromText = false;
 	bool bCreateFromFile = false;
+	bool bTestFirst = false;
 	std::string strInput = "";
+	std::string strHashType = "";
 
-	/*
-	 * just parse the cmdline
-	 */
+	//just parse the commandline
 	for( int i = 1; i < argc ; i++ )
 	{
 		if( strcmp(argv[i],"-s") == 0 )
@@ -85,7 +80,6 @@ int main ( int argc, char **argv)
 			if( i != argc-1 )
 			{
 				strInput = argv[i+1];
-				break;
 			}
 		}
 		else if( strcmp(argv[i],"-f") == 0 )
@@ -94,94 +88,81 @@ int main ( int argc, char **argv)
 			if( i != argc-1 )
 			{
 				strInput = argv[i+1];
-				break;
 			}
 		}
+		else if( strcmp(argv[i],"-h") == 0)
+		{
+			if( i != argc-1 )
+			{
+				strHashType = argv[i+1];
+			}
+		}
+		else if( strcmp(argv[i],"-t") == 0)
+		{
+			bTestFirst = true;
+		}
 	}
 
-	/*
-	 * just print out some little help
-	 * if no options have been submited
-	 */
-	if( (bCreateFromText == false) &&
-	    (bCreateFromFile == false) )
+	// just print out some help if no options have been submited
+	if( ((bCreateFromText == false) && (bCreateFromFile == false))  ||
+	    (strHashType == "") )
 	{
 		printUsage();
-		return 0;
+		return -1;
 	}
-	
+
         //--------------------------------------------------------------
 	
-
-	/*
-	 * We create some wrappers. 
-	 */
-	hashwrapper *md5 = new md5wrapper();
-	hashwrapper *sha1 = new sha1wrapper();
-	hashwrapper *sha256 = new sha256wrapper();
-	hashwrapper *sha384 = new sha384wrapper();
-	hashwrapper *sha512 = new sha512wrapper();
-
-	/*
-	 * these strings will hold our hashes
-	 */
-	std::string sMD5 = "";
-	std::string sSHA1 = "";
-	std::string sSHA256 = "";
-	std::string sSHA384 = "";
-	std::string sSHA512 = "";
-
-	if( bCreateFromText )
+	//Create the wrapper using a simple factory-method
+	hashwrapper *hw = factoryMethod(strHashType);
+	if( hw != NULL)
 	{
-		/*
-		 * producing hashes from the given string
-		 */
-		sMD5 = md5->getHashFromString(strInput);
-		sSHA1 = sha1->getHashFromString(strInput);
-		sSHA256 = sha256->getHashFromString(strInput);
-		sSHA384 = sha384->getHashFromString(strInput);
-		sSHA512 = sha512->getHashFromString(strInput);
-	}
-	else if( bCreateFromFile )
-	{
-		/*
-		 * producing hashes from the given file
-		 */
 		try
 		{
-			sMD5 = md5->getHashFromFile(strInput);
-			sSHA1 = sha1->getHashFromFile(strInput);
-			sSHA256 = sha256->getHashFromFile(strInput);
-			sSHA384 = sha384->getHashFromFile(strInput);
-			sSHA512 = sha512->getHashFromFile(strInput);
+			if(bTestFirst)
+			{
+				//Tests the implemtation. If the test fails on your 
+				//system this will throw a hlException
+				hw->test();
+			}
+			if( bCreateFromText )
+			{
+				//Creates a hash based on the given string
+				std::cout << hw->getHashFromString(strInput) << std::endl;
+			}
+			else if( bCreateFromFile )
+			{
+				//Create a hash based on the given file
+				//This could throw a hlException if the given 
+				//file does not exist
+				std::cout << hw->getHashFromFile(strInput) << std::endl;
+			}
 		}
-		catch (hlException &e)
+		catch(hlException &e)
 		{
-			std::cerr << "Error(" 
+			std::cerr << std::endl;
+			std::cerr << "Error (" 
 				  << e.error_number()
-				  << "): "
+				  << "): " 
 				  << e.error_message()
 				  << std::endl;
-			return e.error_number();
+			delete hw;
+			return -2;
 		}
 	}
+	else
+	{
+		std::cerr << std::endl;
+		std::cerr << "Error: Unknown hashtype \""
+			  << strHashType  
+			  << "\"."
+			  << std::endl;
 
-	/*
-	 * output
-	 */
-	std::cout << "Your hashes: " << std::endl; 
-	std::cout << sMD5 << "(MD5)" << std::endl;
-	std::cout << sSHA1 << "(SHA1)" << std::endl;
-	std::cout << sSHA256 << "(SHA256)" << std::endl;
-	std::cout << sSHA384 << "(SHA384)" << std::endl;
-	std::cout << sSHA512 << "(SHA512)" << std::endl;
+		printUsage();
+		return -1;
+	}
 
-	delete md5;
-	delete sha1;
-	delete sha256;
-	delete sha384;
-	delete sha512;
-
+	delete hw;
 	return 0;
 }
 
@@ -189,14 +170,37 @@ int main ( int argc, char **argv)
 
 void printUsage (void)
 {
-	std::cout << std::endl;
-	std::cout << "hashlib++ example program." << std::endl;
-	std::cout << "This software produces all supported " << std::endl;
-	std::cout << "hashes from a given string or file." << std::endl;
-	std::cout << std::endl;
-	std::cout << "usage: " << std::endl;
-	std::cout << "[-s string] [-f filename]" << std::endl;
-	std::cout << std::endl;
+	std::cerr << std::endl;
+	std::cerr << "hashlib++ example program." << std::endl;
+	std::cerr << "This is a short example program which shows how to use hashlib++." << std::endl;
+	std::cerr << "It produces hash-values for a given string or file." <<std::endl;
+	std::cerr << std::endl;
+	std::cerr << "usage: " << std::endl;
+	std::cerr << "[-t] [-s STRING | -f FILENAME] [-h HASHTYPE]" << std::endl;
+	std::cerr << std::endl;
+	std::cerr << "-t\t Tests the implementation on your system before." << std::endl;
+	std::cerr << "-s\t Creates a hash from a given string." << std::endl;
+	std::cerr << "-f\t Creates a hash from a given file." << std::endl;
+	std::cerr << "-h\t Hashfunction to use: md5,sha1,sha256,sha384,sha512." << std::endl;
+	std::cerr << std::endl;
+ 	std::cerr << "hashlib++ - a simple hash library for C++" <<std::endl;
+	std::cerr << "Copyright (c) 2007,2008,2009 Benjamin Grüdelbach" <<std::endl;
+	std::cerr << std::endl;
+}
+
+hashwrapper* factoryMethod(std::string htype)
+{
+	if( htype == "md5" )
+		return new md5wrapper();
+	else if( htype == "sha1")
+		return new sha1wrapper();
+	else if( htype == "sha256")
+		return new sha256wrapper();
+	else if( htype == "sha384")
+		return new sha384wrapper();
+	else if( htype == "sha512")
+		return new sha512wrapper();
+	return NULL;
 }
 
 //----------------------------------------------------------------------	
